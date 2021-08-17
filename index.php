1a946603c2747ca86de7bdee9f5906d0ad60ddc2
berryboylb <phemmynesce4life@gmail.com>
2021-08-17 23:52:55 +0100

    initial commit

--- index.php
+++ index.php
@@ -11,8 +11,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <style>
+        a {
+            text-decoration: none;
+            color: #000;
+        }
+        a:hover  {
+            opacity: 0.5;
+        }
+    </style>
 </head>
 <body>
-    <h1><?php echo "Hello my name is ". $name;?></h1>
+    <h1><?php echo "Hello my name is ". $name;?> <a href="https://resume-kohl-eight.vercel.app/">Visit my resume <i class="fas fa-long-arrow-alt-right"></i></a></h1>
 </body>
 </html>
